80271b888bfbba4dae01b3101b542c0a29737994
gerbenvandervries <gerbenvandervries@gmail.com>
2016-07-20 14:41:30 +0200

    .

--- protocols/FastqToSam.sh
+++ protocols/FastqToSam.sh
@@ -21,13 +21,12 @@ makeTmpDir ${unMappedBam}
 tmpunMappedBam=${MC_tmpFile}
 
 	java -Xmx5G -jar ${EBROOTPICARD}/picard.jar FastqToSam \
-	FASTQ=/groups/umcg-gaf/tmp04/rawdata/ngs/run_0006_AH5W7TAFXX/lane1_GGACTCCT_S1_L001_R1_001.fq.gz \
-	FASTQ2=/groups/umcg-gaf/tmp04/rawdata/ngs/run_0006_AH5W7TAFXX/lane1_GGACTCCT_S1_L001_R2_001.fq.gz \
+	FASTQ=${peEnd1BarcodeFqGz} \
+	FASTQ2=${peEnd2BarcodeFqGz} \
 	OUTPUT=${tmpunMappedBam} \
 	SAMPLE_NAME=${externalSampleID} \
 	PLATFORM=illumina
-	
 
-	echo -e "\nFastqToSam finished succesfull. Moving temp files to final.\n\n"
-	mv -f ${tmpunMappedBam} ${unMappedBam}
+echo -e "\nFastqToSam finished succesfull. Moving temp files to final.\n\n"
+mv -f ${tmpunMappedBam} ${unMappedBam}
 
